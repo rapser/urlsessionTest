ba8ae85dffccaa8ca8fa744bac6327da99e31a70
Miguel Angel Tomairo <matomairo@gmail.com>
2020-08-16 17:30:47 -0500

    Initial Commit

---
+++ webserviceTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webserviceTest
+//
+//  Created by miguel tomairo on 8/16/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
